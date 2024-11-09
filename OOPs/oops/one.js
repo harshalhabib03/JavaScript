431dc2d2340945e0e9700f1eb5889dd54e57ac8a
harshalhabib03 <harshalhabib2002@gmail.com>
2024-11-09 12:58:04 +0530

    OOPs

---
+++ OOPs/oops/one.js
@@ -0,0 +1,11 @@
+//empty object
+class Emp{}
+
+let e1 = new emp();
+console.log(e1);
+let e2 = new emp();
+console.log(e2);
+let e3 = new emp();
+console.log(e3);
+let e4 = new emp();
+console.log(e4);
---
+++ OOPs/oops/two.js
@@ -0,0 +1,7 @@
+class Movie{
+    //is a template to contruct objects.
+    //once object is created, memory location is allocated.
+}
+
+let m1= new Movie()
+console.log(m1);
